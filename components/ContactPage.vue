63182b538f3ea72fdab491f182f0aaf7fb1eeb86
Omargazzah02 <gazzahomar2001@gmail.com>
2025-08-21 19:56:46 +0100

    second commit

--- components/ContactPage.vue
+++ components/ContactPage.vue
@@ -5,22 +5,22 @@
 <div class="w-full grid grid-cols-1 lg:grid-cols-2 gap-4 lg:px-10">
     
 
-  <a class="border-[var(--secondary-color)] border-2 rounded-md p-4 flex justify-between hover:bg-blue-400 cursor-pointer" href="https://www.linkedin.com/in/omar-gaz/" target="_blank">
+  <a class="border-[var(--secondary-color)] border-2 rounded-md p-4 flex justify-between hover:bg-blue-400 cursor-pointer px-8" href="https://www.linkedin.com/in/omar-gaz/" target="_blank">
     <font-awesome-icon :icon="['fab', 'linkedin']" size="2x" class="text-white" />
     <font-awesome-icon :icon="['fas', 'share']" size="2x" class="text-white" />
   </a>
 
-  <a class="border-[var(--secondary-color)] border-2 rounded-md p-4 flex justify-between hover:bg-blue-400 cursor-pointer" href="https://github.com/Omargazzah02" target="_blank">
+  <a class="border-[var(--secondary-color)] border-2 rounded-md p-4 flex justify-between hover:bg-blue-400 cursor-pointer px-8" href="https://github.com/Omargazzah02" target="_blank">
     <font-awesome-icon :icon="['fab', 'github']" size="2x" class="text-white" />
     <font-awesome-icon :icon="['fas', 'share']" size="2x" class="text-white" />
   </a>
 
-  <a class="border-[var(--secondary-color)] border-2 rounded-md p-4 flex justify-between hover:bg-blue-400 cursor-pointer" href="mailto:gazzahomar2001@gmail.com">
+  <a class="border-[var(--secondary-color)] border-2 rounded-md p-4 flex justify-between hover:bg-blue-400 cursor-pointer px-8" href="mailto:gazzahomar2001@gmail.com">
     <font-awesome-icon :icon="['fas', 'envelope']" size="2x" class="text-white" />
     <font-awesome-icon :icon="['fas', 'share']" size="2x" class="text-white" />
   </a>
 
-  <a class="border-[var(--secondary-color)] border-2 rounded-md p-4 flex justify-between hover:bg-blue-400 cursor-pointer" href="tel:+330767708603">
+  <a class="border-[var(--secondary-color)] border-2 rounded-md p-4 flex justify-between hover:bg-blue-400 cursor-pointer px-8" href="tel:+330767708603">
     <font-awesome-icon :icon="['fas', 'phone']" size="2x" class="text-white" />
     <font-awesome-icon :icon="['fas', 'share']" size="2x" class="text-white" />
   </a>
@@ -33,7 +33,7 @@
     <div  class="w-full lg:p-7 flex  flex-col justify-between items-center lg:px-10  text-sm text-white mt-8 gap-3  max-lg:text-xs  text-center"> 
          <p>© 2025 Omar Gazzah. Tous droits réservés. <img src="" alt=""></p>
 
-     <span class="flex space-x-3 items-center  max-lg:flex-col max-lg:space-y-2">   <p>Site développé avec Next.js et Tailwind CSS. </p> <span class="flex space-x-3"> <img src="/images/next.png" class=" w-6 h-6"/>  <img src="/images/tailwind.png" class=" w-8"/> </span> </span>
+     <span class="flex space-x-3 items-center  max-lg:flex-col max-lg:space-y-2 ">   <p>Site développé avec Nuxt.js et Tailwind CSS. </p> <span class="flex space-x-3"> <img src="/images/nuxt.png" class=" w-8"/>  <img src="/images/tailwind.png" class=" w-7 h-6"/> </span> </span>
         </div>
 
 </template>
