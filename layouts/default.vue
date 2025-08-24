bcc24e9575f733332b33e784760f9f698a6ff5e4
Omargazzah02 <gazzahomar2001@gmail.com>
2025-08-24 21:53:58 +0100

    Adjusting margin

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,7 +4,7 @@
                           <div class="absolute w-28  z-10   top-8 left-30 max-lg:left-8  max-lg:w-20">   <img src="/images/emoji.png" alt="">  </div>
 
 
-                      <div class="absolute w-40  z-10 max-lg:-top-5   right-8 cube-rotation  max-lg:w-36"  > <img src="/images/cube-blue.png" alt=""></div>
+                      <div class="absolute w-40  z-10 max-lg:-top-5   right-10 cube-rotation  max-lg:w-36"  > <img src="/images/cube-blue.png" alt=""></div>
                       <div class="absolute  w-30  z-10  bottom-5 left-6 cube-rotation-reverse max-lg:hidden"  > <img src="/images/cube-blue.png" alt=""></div>
                       <div class="absolute  w-20  z-10  -bottom-15 left-30 cube-rotation max-lg:hidden"  > <img src="/images/cube-gray.png" alt=""></div>
 
